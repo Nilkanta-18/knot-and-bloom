ef489eec2bd9d1d0136d97ce7c3acf75a81ab86c
Nilkanta-18 <nilkantaghosh018@gmail.com>
2026-09-04 21:10:09 +0530

    Fix WhatsApp order link in order function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function order(product, price) {
   const message = `Hi Knot & Bloom! I'd like to order the ${product} (${price}).`;
 
   window.open(
-    'https://wa.me/919999999999?text=' + encodeURIComponent(message),
+    'https://wa.me/9136503166?text=' + encodeURIComponent(message),
     '_blank'
   );
 }
